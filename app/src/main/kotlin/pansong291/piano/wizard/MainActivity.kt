ad283cb40a739578292e26a4fcf8f20337a27a11
pansong291 <pansong291@sina.com>
2024-09-21 18:49:12 +0800

    引入 XXPermissions

--- app/src/main/kotlin/pansong291/piano/wizard/MainActivity.kt
+++ app/src/main/kotlin/pansong291/piano/wizard/MainActivity.kt
@@ -1,20 +1,41 @@
 package pansong291.piano.wizard
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.hjq.permissions.OnPermissionCallback
+import com.hjq.permissions.Permission
+import com.hjq.permissions.XXPermissions
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var btnMain: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        btnMain = findViewById(R.id.btn_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        btnMain.setOnClickListener {
+            XXPermissions.with(this)
+                .permission(Permission.MANAGE_EXTERNAL_STORAGE)
+                .request(object : OnPermissionCallback {
+                    override fun onGranted(permissions: MutableList<String>, allGranted: Boolean) {
+                        Toast.makeText(this@MainActivity, "获取文件读写权限成功", Toast.LENGTH_SHORT).show()
+                    }
+
+                    override fun onDenied(permissions: MutableList<String>, doNotAskAgain: Boolean) {
+                        Toast.makeText(this@MainActivity, "获取文件读写权限失败", Toast.LENGTH_SHORT).show()
+                    }
+                })
         }
     }
 }
