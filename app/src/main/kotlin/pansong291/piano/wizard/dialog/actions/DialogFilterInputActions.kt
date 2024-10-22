6f88176ff6f0a6e771248d7f49fe191d4701be31
pansong291 <pansong291@sina.com>
2024-10-22 17:30:43 +0800

    输入框失焦时收起输入法

--- app/src/main/kotlin/pansong291/piano/wizard/dialog/actions/DialogFilterInputActions.kt
+++ app/src/main/kotlin/pansong291/piano/wizard/dialog/actions/DialogFilterInputActions.kt
@@ -1,16 +1,20 @@
 package pansong291.piano.wizard.dialog.actions
 
+import android.content.Context
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.LinearLayout
 import pansong291.piano.wizard.R
 import pansong291.piano.wizard.dialog.base.IDialog
 
+
 object DialogFilterInputActions {
     fun loadIn(dialog: IDialog, block: (ok: Button, cancel: Button, input: EditText) -> Unit) {
+        val context = dialog.getContext()
         val actions = View.inflate(
-            dialog.getContext(),
+            context,
             R.layout.dialog_actions_filter_input,
             dialog.findActionsWrapper()
         )
@@ -30,6 +34,13 @@ object DialogFilterInputActions {
                 frame.visibility = View.VISIBLE
             }
         }
+        input.setOnFocusChangeListener { v, hasFocus ->
+            if (!hasFocus) {
+                (context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager)
+                    .hideSoftInputFromWindow(v.windowToken, 0)
+            }
+        }
+
         cancel.setOnClickListener { dialog.destroy() }
         block.invoke(ok, cancel, input)
     }
